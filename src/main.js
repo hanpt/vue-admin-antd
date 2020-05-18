29b615c842769dd53572fa99eafd49fdd54401f7
hanpt <345400675@qq.com>
2020-05-18 22:45:57 +0800

    chore: set import on demand

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import { Button } from 'ant-design-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+Vue.component(Button.name, Button);
 
 Vue.config.productionTip = false;
 
