bd42e5474adf40430ab5209339068cdf16eee3cd
hanpt <345400675@qq.com>
2020-06-02 22:18:36 +0800

    feat: add login rules check

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 
 <style lang="less">
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="login">
-    <a-form-model class="login-form" :model="loginForm" @submit.native.prevent>
-      <a-form-model-item>
+    <a-form-model ref="loginForm" class="login-form" :model="loginForm" :rules="rules">
+      <a-form-model-item prop="user">
         <a-input v-model="loginForm.user" placeholder="用户名" allow-clear>
           <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
         </a-input>
       </a-form-model-item>
-      <a-form-model-item>
+      <a-form-model-item prop="password">
         <a-input-password v-model="loginForm.password" type="password" placeholder="密码" allow-clear>
           <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)" />
         </a-input-password>
@@ -15,7 +15,7 @@
         <a-button
           class="login-form-button"
           type="primary"
-          html-type="submit"
+          @click="handleSubmit"
         >
           登录
         </a-button>
@@ -31,19 +31,42 @@ export default {
         user: '',
         password: '',
       },
+      rules: {
+        user: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+        ],
+      },
     }
   },
   methods: {
-    handleSubmit(e) {
-      console.log('e: ', e)
+    handleSubmit() {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          console.log(this.loginForm)
+        } else {
+          console.log('error submit!!')
+        }
+      })
     },
   },
 }
 </script>
 
 <style lang="less" scoped>
+.login {
+  height: 100%;
+  position: relative;
+  background: #002140;
+}
 .login-form {
-  max-width: 300px;
+  width: 300px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
 .login-form-forgot {
   float: right;
